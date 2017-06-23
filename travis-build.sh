0c0015c66b2b2dc0566da69b4d3b6d3d39d3ad2c
Eric Brown <eric.d.brown23@gmail.com>
2017-06-23 17:34:48 -0400

    This will all be worth it

--- travis-build.sh
+++ travis-build.sh
@@ -6,6 +6,8 @@ git clone https://github.com/facebook/folly.git
 
 pushd folly
 git checkout $FOLLY_VERSION
+
+# sed the script to pull glog from the right place
 ./folly/build/deps_ubuntu_12.04.sh
 popd
 
@@ -32,3 +34,50 @@ if [ ! -e "${INSTALL_PREFIX}/lib/libamqpcpp.so" ]; then
     ldconfig
     popd
 fi
+
+# libuv
+LIBUV_VERSION="1.6.1"
+LIBUV_DIR_NAME="libuv-v${LIBUV_VERSION}"
+LIBUV_ARCHIVE_NAME="libuv-v${LIBUV_VERSION}.tar.gz"
+LIBUV_URL="http://dist.libuv.org/dist/v${LIBUV_VERSION}/${LIBUV_ARCHIVE_NAME}"
+if [ ! -e "${INSTALL_PREFIX}/lib64/libuv.so" ] && [ ! -e "${INSTALL_PREFIX}/lib/libuv.so" ]; then
+    if [ ! -e "${WORKDIR}/${LIBUV_ARCHIVE_NAME}" ]; then
+        mkdir -p "${WORKDIR}/download"
+        wget -q -P "${WORKDIR}/download" "${LIBUV_URL}"
+        mv "${WORKDIR}/download/${LIBUV_ARCHIVE_NAME}" "${WORKDIR}"
+    fi
+    tar -C "${WORKDIR}" -zxf "${WORKDIR}/${LIBUV_ARCHIVE_NAME}"
+    pushd "${WORKDIR}/${LIBUV_DIR_NAME}"
+    sh autogen.sh
+    ./configure --prefix="${INSTALL_PREFIX}"
+    ${MAKE}
+    ${MAKE} install
+    popd
+fi
+
+echo "Finished libuv"
+
+# yaml-cpp
+YAML_CPP_VERSION="0.5.2"
+YAML_CPP_DIR_NAME="yaml-cpp-release-${YAML_CPP_VERSION}"
+YAML_CPP_ARCHIVE_NAME="release-${YAML_CPP_VERSION}.tar.gz"
+YAML_CPP_URL="https://github.com/jbeder/yaml-cpp/archive/${YAML_CPP_ARCHIVE_NAME}"
+
+if [ ! -e "${INSTALL_PREFIX}/lib/libyaml-cpp.a" ]; then
+    if [ ! -e "${WORKDIR}/${YAML_CPP_ARCHIVE_NAME}" ]; then
+        mkdir -p "${WORKDIR}/download"
+        wget -q -P "${WORKDIR}/download" "${YAML_CPP_URL}"
+        mv "${WORKDIR}/download/${YAML_CPP_ARCHIVE_NAME}" "${WORKDIR}"
+    fi
+    tar -C "${WORKDIR}" -zxf "${WORKDIR}/${YAML_CPP_ARCHIVE_NAME}"
+    pushd "${WORKDIR}/${YAML_CPP_DIR_NAME}"
+    mkdir -p patches
+    cp ${PATCHESDIR}/patches/yaml-cpp/* patches/
+    git apply patches/*
+    cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" .
+    ${MAKE}
+    ${MAKE} install
+    popd
+fi
+
+echo "Finished dependencies"
