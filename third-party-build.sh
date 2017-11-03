924f69cf84e0ca57bb66f4182f52800595bea670
Matt McKay <mckaymatt@gmail.com>
2017-11-03 16:59:37 -0400

    dockerized build env

--- third-party-build.sh
+++ third-party-build.sh
@@ -6,19 +6,30 @@ set -x
 CPU_COUNT=$(lscpu -p | sed '/^#/ d' | wc -l)
 MAKE="make -j${CPU_COUNT}"
 
-WORKDIR=${1-/home/vagrant/tmp/bin}
-INSTALL_PREFIX=${2-/usr}
-PATCHESDIR=${3-/vagrant}
+SCRIPT=$(realpath $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+
+WORKDIR=${1-${SCRIPTPATH}/workdir}
+INSTALL_PREFIX=${2-/usr/local}
+PATCHESDIR=${3-${SCRIPTPATH}/patches}
+
+
+# Sanity check that the patches dir exists
+if [ ! -e "${PATCHESDIR}" ]; then
+    echo "PATCHESDIR dir does not exist at ${PATCHESDIR}"
+    exit 1
+fi
+
 
 BOOST_MAJOR="1"
-BOOST_MINOR="61"
-BOOST_PATCH="0"
+BOOST_MINOR="65"
+BOOST_PATCH="1"
 BOOST_VERSION="${BOOST_MAJOR}_${BOOST_MINOR}_${BOOST_PATCH}"
 BOOST_VERSION_DOTTED="${BOOST_MAJOR}.${BOOST_MINOR}.${BOOST_PATCH}"
 BOOST_DIR_NAME="boost_${BOOST_VERSION}"
 BOOST_ARCHIVE_NAME="boost_${BOOST_VERSION}.tar.gz"
 BOOST_URL="http://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION_DOTTED}/${BOOST_ARCHIVE_NAME}"
-if [ ! -e "${INSTALL_PREFIX}/lib/libboost_system.so" ]; then
+if [ ! -e "${INSTALL_PREFIX}/lib64/libboost_system.so" ] && [ ! -e "${INSTALL_PREFIX}/lib/libboost_system.so" ]; then
     if [ ! -e "${WORKDIR}/${BOOST_ARCHIVE_NAME}" ]; then
         mkdir -p "${WORKDIR}/download"
         wget -q -P "${WORKDIR}/download" "${BOOST_URL}"
@@ -75,7 +86,7 @@ fi
 
 
 # folly
-FOLLY_VERSION="e28213bf2c5aa1c400cfd2ab363abfeadee81fa2"
+FOLLY_VERSION="017cae98c3f57090c998110042f4844653a86bc0"
 FOLLY_DIR_NAME="folly-${FOLLY_VERSION}"
 FOLLY_ARCHIVE_NAME="v${FOLLY_VERSION}.tar.gz"
 FOLLY_URL="https://github.com/facebook/folly/archive/v${FOLLY_VERSION}.tar.gz"
@@ -87,9 +98,11 @@ if [ ! -e "${INSTALL_PREFIX}/lib64/libfolly.so" ] && [ ! -e "${INSTALL_PREFIX}/l
     fi
     pushd "${WORKDIR}/folly"
     git checkout "${FOLLY_VERSION}"
-    mkdir -p patches
-    cp ${PATCHESDIR}/patches/folly/* patches/
-    git apply patches/*
+    if [ -e "${PATCHESDIR}/folly" ]; then
+        mkdir -p patches
+        cp ${PATCHESDIR}/folly/* patches/
+        git apply patches/*
+    fi
     pushd folly
     autoreconf -ivf
     ./configure --prefix="${INSTALL_PREFIX}"
@@ -120,7 +133,7 @@ if [ ! -e "${INSTALL_PREFIX}/bin/protoc" ]; then
 fi
 
 # yaml-cpp
-YAML_CPP_VERSION="0.5.2"
+YAML_CPP_VERSION="0.5.3"
 YAML_CPP_DIR_NAME="yaml-cpp-release-${YAML_CPP_VERSION}"
 YAML_CPP_ARCHIVE_NAME="release-${YAML_CPP_VERSION}.tar.gz"
 YAML_CPP_URL="https://github.com/jbeder/yaml-cpp/archive/${YAML_CPP_ARCHIVE_NAME}"
@@ -134,8 +147,10 @@ if [ ! -e "${INSTALL_PREFIX}/lib/libyaml-cpp.a" ]; then
     tar -C "${WORKDIR}" -zxvf "${WORKDIR}/${YAML_CPP_ARCHIVE_NAME}"
     pushd "${WORKDIR}/${YAML_CPP_DIR_NAME}"
     mkdir -p patches
-    cp ${PATCHESDIR}/patches/yaml-cpp/* patches/
-    git apply patches/*
+    if [ -d "${PATCHESDIR}/yaml-cpp" ]; then
+        cp ${PATCHESDIR}/yaml-cpp/* patches/
+        git apply patches/*
+    fi
     cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" .
     ${MAKE}
     ${MAKE} install
