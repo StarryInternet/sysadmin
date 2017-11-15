d7969ad6d6b43ed00ca81709771447b367631876
Matt McKay <mckaymatt@gmail.com>
2017-11-14 19:04:38 -0500

    tag latest on master

--- docker_builds/image_deploy.sh
+++ docker_builds/image_deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
 set +x
 
-TRAVIS_TAG=$1
+TAG=$1
 DOCKER_USERNAME=$2
 DOCKER_PASSWORD=$3
 
 docker login -u="$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push "starryoss/sysadmin-build:${TRAVIS_TAG}"
+docker push "starryoss/sysadmin-build:${TAG}"
