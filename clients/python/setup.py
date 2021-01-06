9da3af5c58c580b9e07356701746164e944bfaf2
Rhyan <rgelinas@starry.com>
2021-01-06 13:18:02 -0500

    Pinning version: py-sysadmin/1.3.0

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -62,7 +62,7 @@ GENERATED_DIR = os.path.join('sysadmin', 'generated')
 
 setup(
     name='py-sysadmin',
-    version='1.2.9',
+    version='1.3.0',
     description='Control sysadmin',
     author='Starry Inc',
     author_email='oss@starry.com',
